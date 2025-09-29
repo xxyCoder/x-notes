d808cc10533799fca5e7b39a0ed409433137dae2
xxyCoder <xxycoder@xxyCoderdeMacBook-Pro.local>
2025-09-29 21:29:35 +0800

    feat: fiber node

---
+++ framework/mini-react/reconciler/fiber.ts
@@ -0,0 +1,49 @@
+import {Key, Props, Ref, Type} from "../shared/ReactTypes"
+import {Flags, NoFlags} from "./fiberFlags"
+import {WorkTag} from "./workTags"
+
+export class FiberNode {
+	type: Type
+	key: Key
+	ref: Ref
+	tag: WorkTag
+
+	stateNode: any
+
+	return: FiberNode | null
+	sibling: FiberNode | null
+	child: FiberNode | null
+
+	pendingProps: Props
+	memoizedProps: Props | null
+	memoizedState: any
+
+	alternate: FiberNode | null
+
+	flags: Flags
+	subFlags: Flags
+	updateQueue: any
+
+	constructor(tag: WorkTag, pendingProps: Props, key: Key) {
+		this.type = null
+		this.key = key
+		this.ref = null
+		this.tag = tag
+
+		this.stateNode = null
+
+		this.return = null
+		this.child = null
+		this.sibling = null
+
+		this.pendingProps = pendingProps
+		this.memoizedProps = null
+		this.memoizedState = null
+		this.alternate = null
+
+		this.flags = NoFlags
+		this.subFlags = NoFlags
+
+		this.updateQueue = null
+	}
+}
--- framework/mini-react/shared/ReactTypes.ts
+++ framework/mini-react/shared/ReactTypes.ts
@@ -4,7 +4,7 @@ export type SingleChildren = ReactElementType | string
 export type Props = Record<string, string> & {
 	children?: SingleChildren | SingleChildren[]
 }
-export type Type = string | ((props?: Props) => ReactElementType)
+export type Type = string | ((props?: Props) => ReactElementType) | null
 export type Action<State> = State | ((prevState: State) => State)
 
 export interface ReactElementType {
